1ccc8a5f36e10096542cf5a8caf7d9e0b3baaff6
Teia Leusten <teia.leusten@proton.me>
2024-07-21 02:27:03 +0300

    Add database connection

--- api/shuttle/src/main.rs
+++ api/shuttle/src/main.rs
@@ -1,11 +1,12 @@
 use axum::{routing::get, Router};
+use shuttle_shared_db::Postgres;
 
 async fn hello_world() -> &'static str {
     "Hello, world!"
 }
 
 #[shuttle_runtime::main]
-async fn main() -> shuttle_axum::ShuttleAxum {
+async fn main(#[Postgres] _pool: sqlx::PgPool) -> shuttle_axum::ShuttleAxum {
     let router = Router::new().route("/", get(hello_world));
     Ok(router.into())
 }
